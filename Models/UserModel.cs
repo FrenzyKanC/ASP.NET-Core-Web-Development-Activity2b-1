de7ff116cfa9849002e78d5e392630dc808daf16
Stephan Kansy <stkansy@gmail.com>
2023-12-18 15:57:19 +0100

    Add project files.

---
+++ Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Web_Development_Activity2b_1.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
